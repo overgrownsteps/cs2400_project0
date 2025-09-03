f04f324c60fe757aa07d38acad37f70c9c829c78
overgrownsteps <zoelizko@gmail.com>
2025-09-03 09:48:38 -0700

    Initial commit

---
+++ CharCount.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CharCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter words: ");
+        String words = scanner.nextLine();
+
+        // Remove spaces and count characters
+        int charCount = words.replace(" ", "").length();
+
+        System.out.println("Total number of characters (excluding spaces): " + charCount);
+        scanner.close();
+    }
+}
