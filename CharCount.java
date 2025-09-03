06c065c5266a4436b090701a0816570169cbfc13
overgrownsteps <zoelizko@gmail.com>
2025-09-03 10:59:08 -0700

    change message

--- CharCount.java
+++ CharCount.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class CharCount {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter words: ");
+        System.out.print("Enter your sentence: ");
         String words = scanner.nextLine();
 
         // Remove spaces and count characters
         int charCount = words.replace(" ", "").length();
 
-        System.out.println("Total number of characters (excluding spaces): " + charCount);
+        System.out.println("Total number of characters in your sentence (excluding spaces): " + charCount);
         scanner.close();
     }
 }
